59bf5443b11f2213e447d39a89d07b6bbad17ca7
piotr <gawronski.p@gmail.com>
2024-12-19 09:32:55 +0100

    Refactor product response assertion in tests
    Replaces `assertJsonStructure` with a more expressive `AssertableJson` approach to validate the response. This increases readability and ensures better flexibility in verifying nested JSON structures.

--- src/tests/Feature/ProductControllerTest.php
+++ src/tests/Feature/ProductControllerTest.php
@@ -1,23 +1,21 @@
 <?php
 
-it('gets a full list of product and asserts correct response structure', function () {
-    $response = $this->get('/products')
-        ->assertOk()
-        ->assertJsonCount(5);
+use Illuminate\Testing\Fluent\AssertableJson;
 
-    $response->assertJsonStructure([
-        '*' => [
-            'sku',
-            'name',
-            'category',
-            'price' => [
-                'discount_percentage',
-                'original',
-                'final',
-                'currency'
-            ]
-        ]
-    ]);
+it('gets a full list of product and asserts correct response structure', function () {
+    $this->get('/products')
+        ->assertOk()
+        ->assertJsonCount(5)
+        ->assertJson(fn (AssertableJson $json) => $json->each(
+            fn (AssertableJson $json) => $json->has('sku')
+            ->has('name')
+                ->has('category')
+                ->has('price')
+                ->has('price.discount_percentage')
+                ->has('price.original')
+                ->has('price.final')
+                ->has('price.currency')
+        ));
 });
 
 it('filters products by given category', function ($category, $expectedCount) {
